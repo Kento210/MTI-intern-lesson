ad169ec6f97bd57dbd693bd49ee5d6f4dc92ee0c
YutaSato0717 <yutayuta0717@icloud.com>
2023-08-21 08:13:36 +0000

    投稿されたデータを表示するAPIの作成（修正）

--- lambda/team1-articles-get-internship.js
+++ lambda/team1-articles-get-internship.js
@@ -16,91 +16,83 @@ exports.handler = async (event, context) => {
     body: JSON.stringify({ message: "" }),
   };
 
-  // 今回は簡易的な実装だが、一般的にはAuthorizationHeaderの値は、Authorization: <type> <credentials>のような形式になります。
-  // https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Authorization#%E6%A7%8B%E6%96%87
   if (event.headers.authorization !== "mtiToken") {
-    response.statusCode = 401;
-    response.body = JSON.stringify({
-      message: "認証されていません。HeaderにTokenを指定してください",
-    });
-
-    return response;
+    return {
+      statusCode: 401,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        message: "認証されていません。HeaderにTokenを指定してください",
+      }),
+    };
   }
 
   const { hasValidQs, param } = createParam(event.queryStringParameters);
 
-  const queryCommnad = new QueryCommand(param);
-  const scanCommand = new ScanCommand(param);
+  const command = hasValidQs ? new QueryCommand(param) : new ScanCommand(param);
+  
   try {
-    const articles = hasValidQs
-      ? (await client.send(queryCommnad)).Items
-      : (await client.send(scanCommand)).Items;
-
-    if (articles.length == 0) {
-      response.body = JSON.stringify({ articles: [] });
-    } else {
-      const unmarshalledArticles = articles.map((item) => unmarshall(item));
-      unmarshalledArticles.sort((a, b) => b.timestamp - a.timestamp);
-      response.body = JSON.stringify({ articles: unmarshalledArticles });
-    }
+    const articles = (await client.send(command)).Items || [];
+    const unmarshalledArticles = articles.map((item) => unmarshall(item));
+    unmarshalledArticles.sort((a, b) => b.timestamp - a.timestamp);
+    
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ articles: unmarshalledArticles }),
+    };
   } catch (e) {
-    response.statusCode = 500;
-    response.body = JSON.stringify({
-      message: "予期せぬエラーが発生しました。",
-      errorDetail: e.toString(),
-    });
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        message: "予期せぬエラーが発生しました。",
+        errorDetail: e.toString(),
+      }),
+    };
   }
-
-  return response;
 };
 
-// qsの内容に応じて、Paramを作成する。
 const createParam = (qs) => {
-  // Query操作には、パーティションキーは必須であり、他は任意となる。
-  // そのためuserIdさえあれば、有効となる。
   const hasValidQs = qs?.userId;
-  if (!hasValidQs) {
+
+  const param = {
+    TableName,
+    Limit: 100,
+  };
+
+  if (hasValidQs) {
+    const { userId, start, end, category } = qs;
+    const queryParam = {
+      ...param,
+      KeyConditionExpression: "userId = :uid and #ts BETWEEN :start AND :end",
+      ExpressionAttributeNames: {
+        "#ts": "timestamp",
+      },
+      ExpressionAttributeValues: {
+        ":uid": userId,
+        ":start": Number.isNaN(parseInt(start)) ? 0 : parseInt(start),
+        ":end": Number.isNaN(parseInt(end)) ? Date.now() : parseInt(end),
+      },
+    };
+
+    if (category) {
+      queryParam.FilterExpression = "category = :category";
+      queryParam.ExpressionAttributeValues[":category"] = category;
+    }
+
+    queryParam.ExpressionAttributeValues = marshall(queryParam.ExpressionAttributeValues);
+
     return {
       hasValidQs,
-      param: {
-        // ↓プロパティ名と変数名が同一の場合は、値の指定を省略できる。
-        TableName, // TableName: TableNameと同じ意味
-        Limit: 100,
-      },
+      param: queryParam,
     };
   }
 
-  const { userId, start, end, category } = qs;
-
-  const queryParam = {
-    TableName, // TableName: TableNameと同じ意味
-    Limit: 100,
-
-    // 一つのキーに複数の条件をつけることはできないため、BETWEEN演算を利用する
-    KeyConditionExpression: "userId = :uid and #ts BETWEEN :start AND :end",
-    // timestampは予約後であるため、プレースホルダ経由じゃないと指定できない。
-    ExpressionAttributeNames: {
-      "#ts": "timestamp",
-    },
-    ExpressionAttributeValues: {
-      ":uid": userId,
-      // startが無効な場合は、0以上という条件にすることで、実質フィルタリング無効化
-      ":start": Number.isNaN(parseInt(start)) ? 0 : parseInt(start),
-      // endが無効な場合は、現在時刻以下という条件にすることで、実質フィルタリング無効化
-      ":end": Number.isNaN(parseInt(end)) ? Date.now() : parseInt(end),
-    },
-  };
-
-  // categoryが存在する場合は別途該当のプロパティを追加する。
-  if (category) {
-    queryParam.FilterExpression = "category = :category";
-    queryParam.ExpressionAttributeValues[":category"] = category;
-  }
-
-  queryParam.ExpressionAttributeValues = marshall(queryParam.ExpressionAttributeValues)
-
-  return {
-    hasValidQs,
-    param: queryParam,
-  };
+  return { hasValidQs, param };
 };
