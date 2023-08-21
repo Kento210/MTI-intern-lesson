666a50b390f385f92af963607ca89ed54e189453
futaba3 <nyokinyoki41aya@gmail.com>
2023-08-21 09:42:03 +0000

    add:新しい記事を投稿する機能を追加

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,3 +1,3 @@
 // 自分のエンドポイントに置き換えてください
-// 今は講師のエンドポイント
-export const baseUrl = 'https://qfsftvoho6.execute-api.ap-northeast-1.amazonaws.com/';
+// 今はサンプルのエンドポイント
+export const baseUrl = 'https://qfsftvoho6.execute-api.ap-northeast-1.amazonaws.com';
